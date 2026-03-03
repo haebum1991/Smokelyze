65225031da819c37a8db78ee77b09c4d025a8e01
haebum1991 <haebum1991@gmail.com>
2026-03-03 07:38:04 -0800

    Updated with new content

--- static/js/loader-handler.js
+++ static/js/loader-handler.js
@@ -117,7 +117,7 @@ export async function loadSourceData(sourceKey, isoDate) {
         if (sourceKey === "wildfire_nifc" && data.features) {
             const stateMap = {
                 "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California",
-                "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "FL": "Florida", "GA": "Georgia",
+                "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "District of Columbia", "FL": "Florida", "GA": "Georgia",
                 "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa",
                 "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland",
                 "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri",
