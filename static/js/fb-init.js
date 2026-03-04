98fc7b2812982aff5d88cb93c3001867e4157ff0
haebum1991 <haebum1991@gmail.com>
2026-03-04 01:17:14 -0800

    Updated with new content

--- static/js/fb-init.js
+++ static/js/fb-init.js
@@ -7,13 +7,13 @@ import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.1.
 // 어차피 fb는 서버내 rule에 통제되므로 key를 노출시켜도 상관이 없다.
 // 이 방식 아니면 아이폰에서는 즉각적 반응을 안함...
 const fbConfig = {
-    apiKey: "AIzaSyCepAjKyOcMqnrtTe7F7IlRciMA7ijLayE",
+    apiKey: "AIzaSyAAQJpMU75J8ZPtB2e3Qx-YJoPQ8AOObVM",
     authDomain: "pmo3smoketool.firebaseapp.com",
     projectId: "pmo3smoketool",
     storageBucket: "pmo3smoketool.firebasestorage.app",
     messagingSenderId: "1068523865415",
-    appId: "1:1068523865415:web:f7a4a74163e5b3c573f862",
-    measurementId: "G-4WR961E7H6"
+    appId: "1:1068523865415:web:55cec0c9057ef07573f862",
+    measurementId: "G-GXTFTTVL5B"
 };
 
 const app = initializeApp(fbConfig);
