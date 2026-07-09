f0a8452c1ec8431c77be5b9ca7207128b1e5d676
haebum1991 <haebum1991@gmail.com>
2026-07-08 19:58:29 -0700

    Updated with new content

--- static/js/map-area-stats.js
+++ static/js/map-area-stats.js
@@ -268,92 +268,47 @@ const DrawRectangle = {
         this.addFeature(rectangle);
         this.clearSelectedFeatures();
         this.updateUIClasses({ mouse: "add" });
-        this.setActionableState({ trash: true });
-
-        // 변경: 맵 고유의 드래그 기능을 완전히 꺼서 MapboxDraw가 이벤트를 먹도록 처리
-        if (map) {
-            map.dragPan.disable();
-            map.touchZoomRotate.disable();
-        }
-
+        this.setActionableState({
+            trash: true
+        });
         return {
             rectangleId: rectangle.id,
-            startPoint: null,
-            drawing: false
+            startPoint: null
         };
     },
 
-    // 변경: 터치/마우스 다운(누르기 시작) 시점을 시작점으로 인지
-    onDrag: function (state, e) {
-        if (!state.startPoint) {
-            state.startPoint = [e.lngLat.lng, e.lngLat.lat];
-            state.drawing = true;
-        }
-
-        const start = state.startPoint;
-        const end = [e.lngLat.lng, e.lngLat.lat];
-        const coords = [
-            [start[0], start[1]],
-            [end[0], start[1]],
-            [end[0], end[1]],
-            [start[0], end[1]],
-            [start[0], start[1]]
-        ];
-
-        const feature = this.getFeature(state.rectangleId);
-        if (feature) {
-            feature.setCoordinates([coords]);
-        }
-    },
-
-    // 추가: 마우스 드래그와 터치 드래그가 동일하게 작동하도록 매핑
-    onTouchMove: function (state, e) {
-        this.onDrag(state, e);
-    },
-
-    // 변경: 마우스나 손가락을 떼는 순간 사각형 완성 및 고정
-    onMouseUp: function (state, e) {
-        if (state.drawing) {
-            this.updateUIClasses({ mouse: "pointer" });
-            this.changeMode("simple_select", { featureIds: [state.rectangleId] });
-            
-            // 맵 원래 기능 원상복구
-            if (map) {
-                map.dragPan.enable();
-                map.touchZoomRotate.enable();
-            }
-        }
-    },
-
-    // 터치가 끝났을 때도 동일하게 마무리
-    onTouchEnd: function (state, e) {
-        this.onMouseUp(state, e);
-    },
-
-    // 데스크톱 클릭 유저를 위한 하이브리드 지원 (클릭-이동-클릭도 같이 보장)
     onClick: function (state, e) {
         if (!state.startPoint) {
             state.startPoint = [e.lngLat.lng, e.lngLat.lat];
-            state.drawing = true;
         } else {
-            this.onMouseUp(state, { lngLat: e.lngLat });
+            this.updateUIClasses({ mouse: "pointer" });
+            this.changeMode("simple_select", { featureIds: [state.rectangleId] });
         }
     },
 
     onMouseMove: function (state, e) {
-        if (state.drawing && !state.startPoint) return;
         if (state.startPoint) {
-            this.onDrag(state, e);
+            const start = state.startPoint;
+            const end = [e.lngLat.lng, e.lngLat.lat];
+            const coords = [
+                [start[0], start[1]],
+                [end[0], start[1]],
+                [end[0], end[1]],
+                [start[0], end[1]],
+                [start[0], start[1]]
+            ];
+            const feature = this.getFeature(state.rectangleId);
+            feature.setCoordinates([coords]);
         }
     },
 
     onKeyUp: function (state, e) {
-        if (e.keyCode === 27) { // ESC
+        if (e.keyCode === 27) { // ESC key
             this.deleteFeature(state.rectangleId, { silent: true });
             this.changeMode("simple_select");
-            if (map) {
-                map.dragPan.enable();
-                map.touchZoomRotate.enable();
+            const drawBtn = document.getElementById("MapBtnDraw");
+            if (drawBtn) {
+                drawBtn.classList.remove("active");
             }
         }
     },
@@ -370,6 +325,16 @@ const DrawRectangle = {
 
 export function initMapDrawStats() {
     injectStyles();
+
+    const isMobileOrTablet = window.innerWidth <= 1024 ||
+        /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent) ||
+        (navigator.maxTouchPoints > 0 && /Macintosh/i.test(navigator.userAgent));
+
+    if (isMobileOrTablet) {
+        console.log("Drawing statistics initialization bypassed on mobile/tablet to optimize tap interaction.");
+        return;
+    }
+
     if (typeof MapboxDraw === "undefined") {
         console.log("Waiting for MapboxDraw library...");
         setTimeout(initMapDrawStats, 50);
--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v-20260708-1954"; // R에서 자동으로 생성된 버전
+const CACHE_NAME = "v-20260708-1958"; // R에서 자동으로 생성된 버전
 const OFFLINE_URL = "/offline/";
 
 const requiredFiles = [
